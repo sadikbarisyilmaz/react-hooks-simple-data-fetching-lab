af09eff8a0ccbc4472aac4654744c44dabf32536
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-06-12 13:35:34 +0300

    completed assignmnet

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [dog, setDog] = useState("");
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => setDog(data.message));
+
+    return () => {};
+  }, []);
+  console.log(dog);
+  return (
+    <div>
+      {dog === "" ? <p>Loading...</p> : <img src={dog} alt="A Random Dog" />}
+    </div>
+  );
+}
+
+export default App;
